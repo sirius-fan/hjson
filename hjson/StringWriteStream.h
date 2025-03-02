fe72cab25ed4cb0be366f87cedfd78adb5e21cee
sirius-fan <hi.fanjialiang@gmail.com>
2025-03-02 13:09:07 +0800

    first commit

---
+++ hjson/StringWriteStream.h
@@ -0,0 +1,34 @@
+#ifndef TJSON_STRINGWRITESTREAM_H
+#define TJSON_STRINGWRITESTREAM_H
+
+#include <string_view>
+#include <vector>
+
+#include <hjson/noncopyable.h>
+
+namespace json
+{
+
+class StringWriteStream: noncopyable
+{
+public:
+    void put(char c)
+    {
+        buffer_.push_back(c);
+    }
+    void put(std::string_view str)
+    {
+        buffer_.insert(buffer_.end(), str.begin(), str.end());
+    }
+    std::string_view get() const
+    {
+        return std::string_view(&*buffer_.begin(), buffer_.size());
+    }
+
+private:
+    std::vector<char> buffer_;
+};
+
+}
+
+#endif //TJSON_STRINGWRITESTREAM_H
---
+++ hjson/noncopyable.h
@@ -0,0 +1,19 @@
+#ifndef TJSON_NONCOPYABLE_H
+#define TJSON_NONCOPYABLE_H
+
+namespace json
+{
+
+class noncopyable
+{
+public:
+    noncopyable(const noncopyable&) = delete;
+    void operator=(const noncopyable&) = delete;
+protected:
+    noncopyable() = default;
+    ~noncopyable() = default;
+};
+
+}
+
+#endif //TJSON_NONCOPYABLE_H
